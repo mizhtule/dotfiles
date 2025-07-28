840858902241e5e60eaa65dff4eb69e424dad85f
Lê Minh Tú <mizhtule@gmail.com>
2025-07-28 21:34:17 +0700

    Add Pyenv configuration

---
+++ dot_config/zsh/dot_zshrc.d/pyenv.zsh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+
+(( $+commands[pyenv] )) || return 1
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init - zsh)"
