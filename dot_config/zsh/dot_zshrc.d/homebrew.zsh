895793a8588fc77641fc8d453abcb4ba18e190b0
Lê Minh Tú <mizhtule@gmail.com>
2025-08-02 20:11:41 +0700

    Add aliases to Homebrew's zshrc

--- dot_config/zsh/dot_zshrc.d/homebrew.zsh
+++ dot_config/zsh/dot_zshrc.d/homebrew.zsh
@@ -1,5 +1,8 @@
 #!/usr/bin/env zsh
 
 (( $+commands[brew] )) || return 1
+
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
+alias brewup="brew update && brew upgrade && brew cleanup --prune=all"
+alias brewinfo="brew leaves | xargs brew desc --eval-al"
